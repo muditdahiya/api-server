61d206ef23507975acf423036c81db506a821a75
muditdahiya <mdtheroxtar@gmail.com>
2023-07-21 22:21:20 -0400

    adding countries and updating count working

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var corsOptions = {
   origin: "*",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
+const axios = require("axios");
 
 //SERVER
 const express = require("express");
@@ -21,12 +22,11 @@ app.use(cors(corsOptions));
 
 //DATABASE
 const { Pool } = require("pg");
-const res = require("express/lib/response");
 
 const pool = new Pool({
   user: "postgres",
   host: "muditdahiya.com",
-  database: "thought_serve",
+  database: "mudit_online",
   password: process.env.PGPASS,
   port: 5432,
 });
@@ -34,6 +34,26 @@ const pool = new Pool({
 pool.connect();
 
 //REQUESTS
+app.get("/", (req, res) => {
+  res.send("Root of api server");
+});
+
+app.post("/updateDemographic", async (req, res) => {
+  const ip = req.body.ip;
+
+  const locationResponse = await axios.get(
+    `https://api.ip2location.io/?key=${process.env.LOCATION_API_KEY}&ip=${ip}`
+  );
+
+  const country = locationResponse.data.country_name;
+
+  //update table using sql
+  pool.query(
+    `INSERT INTO demographic (country, count) VALUES ('${country}', 1) ON CONFLICT (country) DO UPDATE SET count = demographic.count + 1;`
+  );
+
+  res.send(country);
+});
 
 //START SERVER
 app.listen(port, () => {
