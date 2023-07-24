24c84aa802eca3fbe8d3624a2ae1736bac95c62f
muditdahiya <mdtheroxtar@gmail.com>
2023-07-23 22:31:40 -0400

    added end points

--- server.js
+++ server.js
@@ -35,24 +35,27 @@ pool.connect();
 
 //REQUESTS
 app.get("/", (req, res) => {
-  res.send("Root of api server");
+  res.send("Root of API server");
 });
 
 app.post("/updateDemographic", async (req, res) => {
   const ip = req.body.ip;
-
   const locationResponse = await axios.get(
     `https://api.ip2location.io/?key=${process.env.LOCATION_API_KEY}&ip=${ip}`
   );
-
   const country = locationResponse.data.country_name;
-
   //update table using sql
   pool.query(
     `INSERT INTO demographic (country, count) VALUES ('${country}', 1) ON CONFLICT (country) DO UPDATE SET count = demographic.count + 1;`
   );
-
   res.send(country);
+});
+
+app.get("/countryCount", async (req, res) => {
+  const countries = await pool.query(
+    "SELECT * FROM demographic ORDER BY count DESC;"
+  );
+  res.send(countries.rows);
 });
 
 //START SERVER
